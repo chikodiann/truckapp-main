9cd60ab3f4184aaf20508d796cfb2791567f90d7
Chikodinaka Ann Anyanwu <chikodiann@gmail.com>
2024-01-30 10:22:53 +0100

    WebConfig

---
+++ src/main/java/com/ann/truckApp/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.ann.truckApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT","PATCH", "DELETE", "HEAD", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
